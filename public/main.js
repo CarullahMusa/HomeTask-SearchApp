3ad5c4187ae51ccab9fc4babe6609d52950fe138
CarullahMusa <caruniversite@gmail.com>
2024-06-30 22:07:15 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -63,8 +63,6 @@ function addToHtml(info) {
     parent.appendChild(detailsWrapper);
 
     addDetails(info, detailsWrapper)
-
-    informationWrapper.appendChild(dogCard);
 }
 
 function addDetails(info, dWrapper) {
@@ -86,31 +84,31 @@ function addDetails(info, dWrapper) {
         if (key !== 'image_link' && key !== 'name') {
             const endValue = key.replace(/_/g, ' ');  // _ karakterlerini boşlukla değiştir
             const p = document.createElement('p');
-            p.textContent = `${endValue}: ${info[key]}`;  // key yerine endValue kullan
+            p.textContent = `${endValue}: ${info[key]}`;
 
-            if (endValue.split(' ')[0] === "good") {  // endValue'nun ilk kelimesi "Good" ise
-                if (!firstDetails.querySelector('summary')) {  // Eğer firstDetails içinde summary yoksa
+            if (endValue.split(' ')[0] === "good") {
+                if (!firstDetails.querySelector('summary')) {
                     const summary = document.createElement('summary');
                     summary.textContent = "Good with: ";
-                    firstDetails.appendChild(summary);  // summary'i firstDetails içine ekleyin
+                    firstDetails.appendChild(summary);
                 }
-                firstDetails.appendChild(p);  // p elementini firstDetails içine ekleyin
+                firstDetails.appendChild(p);
 
-            } else if (endValue.split(' ')[0] === "min" || endValue.split(' ')[0] === "max") {  // endValue'nun ilk kelimesi "min" veya "max" ise
-                if (!secondDetails.querySelector('summary')) {  // Eğer secondDetails içinde summary yoksa
+            } else if (endValue.split(' ')[0] === "min" || endValue.split(' ')[0] === "max") {
+                if (!secondDetails.querySelector('summary')) {
                     const summary = document.createElement('summary');
                     summary.textContent = "life and siz (min,max) : ";
-                    secondDetails.appendChild(summary);  // summary'i secondDetails içine ekleyin
+                    secondDetails.appendChild(summary);
                 }
-                secondDetails.appendChild(p);  // p elementini secondDetails içine ekleyin
+                secondDetails.appendChild(p);
 
-            } else {  // "Good", "min" veya "max" değilse
-                if (!thirdDetails.querySelector('summary')) {  // Eğer thirdDetails içinde summary yoksa
+            } else {
+                if (!thirdDetails.querySelector('summary')) {
                     const summary = document.createElement('summary');
                     summary.textContent = "behaviours : ";
-                    thirdDetails.appendChild(summary);  // summary'i thirdDetails içine ekleyin
+                    thirdDetails.appendChild(summary);
                 }
-                thirdDetails.appendChild(p);  // p elementini thirdDetails içine ekleyin
+                thirdDetails.appendChild(p);
             }
         }
     }
